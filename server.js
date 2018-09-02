e7320e7f2bed7bab01e7406d7f32f3881448b82a
DroidTechnician <droidtechnicians@gmail.com>
2018-09-02 19:43:49 +0530

    Cors lib added

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const appConfig = require('./constants/config'),
     mongoose = require('mongoose'),
     routes = require('./api/routes/routes'),
     User = require('./api/models/user-model'),
-    env = require('./env.json')[process.env.NODE_ENV || 'development'];
+    env = require('./env.json')[process.env.NODE_ENV || 'development'],
+    cors = require('cors');
 
 // Starting connecting with mongoDB
 mongoose.Promise = global.Promise;
@@ -15,18 +16,7 @@ app.use(bodyParser.urlencoded({ extended: appConfig.urlEncoded }));
 app.use(bodyParser.json());
 
 // Allow CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-        return res.status(200).json({});
-    };
-    next();
-});
+app.use(cors());
 
 // Registering Application routes
 routes(app);
