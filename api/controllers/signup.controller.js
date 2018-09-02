ede94fa30414244e5adfaf3bb3e8ab5921e8fd38
DroidTechnician <droidtechnicians@gmail.com>
2018-09-02 12:49:37 +0530

    Bug issues

--- api/controllers/signup.controller.js
+++ api/controllers/signup.controller.js
@@ -5,7 +5,7 @@ const User = require('../models/user-model'),
 exports.createUser = (req, res) => {
     const newUser = new User(req.body);
     newUser.save((error, user) => {
-        if (error) res.send(errorProcessor(error, res));
+        if (error) res.send(errorProcessor.errorProcessor(error, res));
         else res.send(responseProcessor({ res: "User Created Successfully" }));
     })
 }
@@ -13,8 +13,14 @@ exports.createUser = (req, res) => {
 exports.getUser = (req, res) => {
     const signUp = new SignUpModel(req.body);
     User.find({ 'emailId': signUp.emailId }, (error, user) => {
-        if (error) res.send(errorProcessor(error, res));
-        else res.send(responseProcessor(user[0], ['firstName', 'lastName'], 'No User found'));
+        if (error) res.send(errorProcessor.errorProcessor(error, res));
+        else {
+            if (signUp.password === user[0].password) {
+                res.send(responseProcessor(user[0], ['firstName', 'lastName'], 'No User found'));
+            } else {
+                res.send(errorProcessor.userUnauthorizedError(res));
+            }
+        }
     })
 }
 
