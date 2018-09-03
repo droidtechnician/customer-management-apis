8193a08819fc8e690d3b6152d2935be8cf0e1009
DroidTechnician <droidtechnicians@gmail.com>
2018-09-03 19:56:04 +0530

    Options Request fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
         if (req.method === 'OPTIONS') {
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
             res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
-            return res.status(200).json({});
+            next();
         } else {
             cors();
         }
