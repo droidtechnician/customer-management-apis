cc6eb88282d28402ffe8d448ec6f0d76774d12b1
DroidTechnician <droidtechnicians@gmail.com>
2018-09-03 23:31:41 +0530

    CORS issue fixed

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (req.method === 'OPTIONS') {
         res.header("Access-Control-Allow-Headers", "access-control-allow-origin,content-type");
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
