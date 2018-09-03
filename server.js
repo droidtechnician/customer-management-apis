04c562033e80afe5bac0e3e9d15924509cfea100
DroidTechnician <droidtechnicians@gmail.com>
2018-09-04 01:09:14 +0530

    CORS issue resolved

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Origin', '*');
     if (req.method === 'OPTIONS') res.status(200);
     next();
 });
