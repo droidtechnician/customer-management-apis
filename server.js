b08ac660e417c39738e85448294aa4baf559ac84
DroidTechnician <droidtechnicians@gmail.com>
2018-09-03 22:32:59 +0530

    Options Issue resolved

--- server.js
+++ server.js
@@ -17,15 +17,18 @@ app.use(bodyParser.json());
 
 // Allow CORS
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     if (req.method === 'OPTIONS') {
+
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Credentials", true);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
         res.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin,content-type");
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
         res.status(200).json({});
-    };
+    } else {
+        cors();
+    }
     next();
 });
 
