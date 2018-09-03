496d1f59b3f48978e325ee111dc3d76f1c957475
DroidTechnician <droidtechnicians@gmail.com>
2018-09-04 00:33:54 +0530

    CORS issue resolved

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 // Allow CORS
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://droidtechnician.github.io");
     res.header("Access-Control-Allow-Credentials", true);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
