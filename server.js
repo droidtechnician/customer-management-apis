b8ac623138095d5ceac33809d95843975fb00d5b
DroidTechnician <droidtechnicians@gmail.com>
2018-09-03 19:58:07 +0530

    Options Issue resolved

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.json());
 app.use(
     (req, res, next) => {
         if (req.method === 'OPTIONS') {
+            res.header("Access-Control-Allow-Origin", "*");
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
             res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
             next();
